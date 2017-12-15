dfee17ca2b0db232d0d0f2c500c15f6c2305e7b7
selmanbey <selmannur@yahoo.com>
2017-12-15 05:51:30 +0000

    tiny stylistic changes

--- week8/hangman.js
+++ week8/hangman.js
@@ -10,7 +10,7 @@ $('document').ready( function() {
   'official', 'palace', 'plate', 'poetry', 'policeman', 'positive', 'possible',
   'practical', 'pride', 'promise', 'recall', 'relationship', 'remarkable',
   'require', 'rhyme', 'rocky', 'rush', 'sale', 'satellite', 'satisfaction',
-  'scary', 'selection', 'shake', 'shallow', 'shout', 'silly', 'simple',
+  'scary', 'selection', 'shake', 'shallow', 'shout', 'silly', 'simples',
   'slight', 'slip', 'slope', 'soap', 'solar', 'species', 'spin', 'stiff',
   'swung', 'tale', 'thumb', 'tobacco', 'toy', 'tray', 'tune', 'university',
   'vapor', 'vessels', 'wealth', 'wolf', 'zoo'];
@@ -48,24 +48,25 @@ $('document').ready( function() {
       var guessedLetter = "";
       var input = $(this).val();
      if (/^[a-zA-Z]$/.test(input)) {
+      // console.log("got you!")
       guessedLetter = input.toUpperCase();
 
       if (repeatingLetters.includes(guessedLetter)) {
         notificationBoard.html("You already guessed this letter!");
       } else {
         repeatingLetters.push(guessedLetter);
-
         if (chosenWord == "") {
         } else if (chosenWord.includes(guessedLetter)) {
+          // console.log("before guessedWord is: ", guessedWord)
           chosenWord.forEach (function (currentLetter) {
-
+           // while (chosenWord.includes(guessedLetter)) {
            if (guessedLetter === currentLetter) {
              var index = chosenWord.indexOf(currentLetter);
              chosenWord[index] = "_";
              guessedWord[index] = guessedLetter;
              wordBoard.html(printBoard(guessedWord));
            };
-
+           // };
           })
         } else {
           chances = chances - 1;
@@ -79,6 +80,25 @@ $('document').ready( function() {
 
       $(this).val("")
 
+      switch (chances) {
+        case 4:
+          $("span").css("color", "#229954");
+          $("input").css("border-color", "#229954");
+          break;
+        case 3:
+          $("span").css("color", "#F1C40F");
+          $("input").css("border-color", "#F1C40F");
+          break;
+        case 2:
+          $("span").css("color", "#FF851B");
+          $("input").css("border-color", "#FF851B");
+          break;
+        case 1:
+          $("span").css("color", "#C70039");
+          $("input").css("border-color", "#C70039");
+          break;
+      };
+
       if (!guessedWord.includes("_")) {
         $(".gamewon").css("display", "block");
         $(".gameon").css("display", "none");
@@ -88,6 +108,26 @@ $('document').ready( function() {
         $(".gameon").css("display", "none");
         $(".word_revealed").html("The word was: ".concat(randomWord.toUpperCase()));
       };
+
+
+
+    // console.log(guessedLetter)
     };
   });
+
+
+
+
+  // function validate(input) {
+  //   if (/^[a-zA-Z]$/.test(input)) {
+  //     // console.log("got you!")
+  //     return input.toLowerCase();
+  //  } else {
+  //     alert("A letter and only one letter please!")
+  //  }
+  // }
+
+
+
+  // $('#board').HTML(displayWord)
 });
