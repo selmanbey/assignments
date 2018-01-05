c3712acccd07f5ca227869112fe82bcb03af0b54
selmanbey <selmannur@yahoo.com>
2018-01-05 21:49:24 +0000

    targets working

--- week12/main.js
+++ week12/main.js
@@ -46,45 +46,110 @@ document.addEventListener("DOMContentLoaded", function(){
         for (let i=0; i < row; i++) {
             tableInnerHTML += "<tr id=\"" + String(i + 1) + "\">\n";
             for (let j=0; j < col; j++) {
-                tableInnerHTML += "\t<td id=\"" + String(i + 1) + String(j + 1) + "\">" + String(gameNumbers[i][j]) + "</td>\n";
+                tableInnerHTML += "\t<td id=\"" + String(i + 1) + String(j + 1) + "\" class=\"active_cell\">" + String(gameNumbers[i][j]) + "</td>\n";
             };
+            tableInnerHTML += "<td class=\"empty\"></td>"
+            tableInnerHTML += "<td id=\"targetrow" + String(i + 1) + "\" class=\"target\"></td>"
             tableInnerHTML += "</tr>\n";
         };
+
+        tableInnerHTML += "<tr class=\"empty\"></tr>"
+
+        for (let n=0; n < col; n++) {
+            tableInnerHTML += "<td id=\"targetcol" + String(n + 1) + "\" class=\"target\">\n";
+        }
 
         return tableInnerHTML;
     };
 
-    let gameNumbers = getGameNumbers(row, col);
-    let gameNumbersByCol = getGameNumbersByCol(gameNumbers);
-    // console.log(gameNumbers)
-    // console.log(gameNumbersByCol)
+    function checkTargetsForRows(gameNumbers) {
+        for(let i=0; i < gameNumbers.length; i++) {
+            let total = 0
+            for (let j=0; j < gameNumbers[i].length; j++) {
+                total += gameNumbers[i][j]
+            }
+            elementID = "targetrow" + String(i+1)
+            let cell = document.getElementById(elementID)
+            cell.innerHTML = total 
+        }        
+    } 
 
-    table.innerHTML = createTableInnerHTML(row, col, gameNumbers);
-    
+    function checkTargetsForCols(gameNumbersByCol) {
+        for(let i=0; i < gameNumbersByCol.length; i++) {
+            let total = 0
+            for (let j=0; j < gameNumbersByCol[i].length; j++) {
+                total += gameNumbersByCol[i][j]
+            }
+            elementID = "targetcol" + String(i+1)
+            let cell = document.getElementById(elementID)
+            cell.innerHTML = total 
+        }   
+    }
 
-    function visualiseCancellation() {
-        if (this.className == "inactive_cell") {
-            this.className = "active_cell"
-        } else {
-            this.className = "inactive_cell"
+    function checkAllTheTargets(gameNumbers, gameNumbersByCol) {
+        checkTargetsForRows(gameNumbers);
+        checkTargetsForCols(gameNumbersByCol);
+    }
+
+    function visualiseCancellation(event) {
+        cellID = String(event.id);
+
+        if (event.className == "inactive_cell") {
+            number = gameNumbers[parseInt(cellID[0])-1][parseInt(cellID[1]-1)];
+            dynamicGameNumbers[parseInt(cellID[0])-1][parseInt(cellID[1]-1)] = number;
+            dynamicGameNumbersByCol[parseInt(cellID[1])-1][parseInt(cellID[0]-1)] =number;
+            event.className = "active_cell";
+        } else if (event.className == "active_cell") {   
+            dynamicGameNumbers[parseInt(cellID[0])-1][parseInt(cellID[1]-1)] = 0;
+            dynamicGameNumbersByCol[parseInt(cellID[1])-1][parseInt(cellID[0]-1)] = 0
+            event.className = "inactive_cell";
         }
     }
 
 
     function cancelNumber() {
-        visualiseCancellation();
+        event = this;
+        visualiseCancellation(event);
+        // toggleNumber(event, gameNumbers, dynamicGameNumbers);
+        checkAllTheTargets(dynamicGameNumbers, dynamicGameNumbersByCol)
     }
 
+    function toggleNumber(event, gameNumbers, dynamicGameNumbers) {
+        cellID = String(event.id);
+        
+        if (event.className == "inactive_cell") {
+            dynamicGameNumbers[parseInt(cellID[0])-1][parseInt(cellID[1]-1)] = 0;
+            console.log("active cell clicked")
+            console.log(dynamicGameNumbers[parseInt(cellID[0])-1][parseInt(cellID[1]-1)])
+        } else if (event.className == "active_cell") {
+            number = gameNumbers[parseInt(cellID[0])-1][parseInt(cellID[1]-1)];
+            console.log("inactive cell clicked")
+            console.log("number", number)
+            dynamicGameNumbers[parseInt(cellID[0])-1][parseInt(cellID[1]-1)] = number;
+            console.log("dynamic", dynamicGameNumbers[parseInt(cellID[0])-1][parseInt(cellID[1]-1)])
+        }
+
+    }
+
+
+    /// THE GAME //////////////////////////////////////////////////////////////
+
+    let gameNumbers = getGameNumbers(row, col);
+    let gameNumbersByCol = getGameNumbersByCol(gameNumbers);
+    let dynamicGameNumbers = JSON.parse(JSON.stringify(gameNumbers));
+    let dynamicGameNumbersByCol = JSON.parse(JSON.stringify(gameNumbersByCol));
+
+    table.innerHTML = createTableInnerHTML(row, col, gameNumbers); 
+
     var allTableCells = document.querySelectorAll("td")
-    console.log(typeof allTableCells)
-    console.log(allTableCells)
     
     allTableCells.forEach(function(e){
         e.addEventListener("click", cancelNumber);
     });
     
 
-
+    checkAllTheTargets(gameNumbers, gameNumbersByCol)
+        
     
 
 
